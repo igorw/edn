9b15efb20e34682116074acdc69810198eab8612
Igor Wiedler <igor@wiedler.ch>
2013-03-14 21:06:51 +0100

    Rename pretty_print.php to util.php

--- examples/parse.php
+++ examples/parse.php
@@ -1,7 +1,7 @@
 <?php
 
 require __DIR__.'/../vendor/autoload.php';
-require __DIR__.'/pretty_print.php';
+require __DIR__.'/util.php';
 
 $edn = file_get_contents('examples/sample.edn');
 $data = igorw\edn\parse($edn);
